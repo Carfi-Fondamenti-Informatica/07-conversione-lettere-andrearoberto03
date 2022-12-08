c378ff7742e16aef78bdb95ef7e308d6e6e81572
andrearoberto03 <116633548+andrearoberto03@users.noreply.github.com>
2022-12-08 23:55:39 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,1 +1,10 @@
+//
+// Created by asus on 08/12/2022.
+//
+
+#ifndef UNTITLED1_LIBRERIAAA_H
+#define UNTITLED1_LIBRERIAAA_H
+bool carattere(int x);
+char conversione(char x);
+#endif //UNTITLED1_LIBRERIAAA_H
 
