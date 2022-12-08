5e3aaa205e5a39da30de80cbed951d09d8ce1872
andrearoberto03 <116633548+andrearoberto03@users.noreply.github.com>
2022-12-09 00:23:00 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,22 +1,18 @@
-//
-// Created by asus on 08/12/2022.
-//
-
 #include "libreriaaa.h"
 bool carattere(int x) {
-    if ((x>=65&&x<=90) || (x>=97&&x<=122)) {
+    if (x>=65 && x<=90||x>=97 && x<=122) {
         return true;
-    } else {
+    } else 
         return false;
-    }
+    
 }
 
 char conversione(char x){
     char z;
-    if(x >= "a" && x <= "z"){
-        z=x-32;
-    }else if(x>="A" && x<="Z"){
+    if(x >= 'A' && x <= 'Z'){
         z=x+32;
+    }else if(x>='a' && x<='z'){
+        z=x-32;
     }
     return z;
 }
